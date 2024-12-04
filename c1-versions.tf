666f1b3dafeff1289b5997d1c8e93058cc6df74d
Mr Nim <mr.nim94@gmail.com>
2024-12-04 15:34:12 +0000

    initial project

---
+++ c1-versions.tf
@@ -0,0 +1,34 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+     }
+    helm = {
+      source = "hashicorp/helm"
+      #version = "2.4.1"
+      version = "~> 2.4"
+    }
+    http = {
+      source = "hashicorp/http"
+      #version = "2.1.0"
+      version = "~> 3.0"
+    }
+  }
+#   # Adding Backend as S3 for Remote State Storage
+#   backend "s3" {
+#      bucket = "private-windows-mdaas-eks-tf-lock"
+#      key    = "fsx-lustre.tfstate"
+#      region = "us-east-1" 
+
+#      # For State Locking
+#      dynamodb_table = "private-windows-mdaas-eks-tf-lock"    
+#   }     
+}
+
+#Terraform AWS Provider Block
+provider "aws" {
+  region = var.aws_region
+}
---
+++ c4-02-fsx-lustre-csi-install-using-helm.tf
@@ -0,0 +1,15 @@
+# Install EBS CSI Driver using HELM
+# Resource: Helm Release 
+resource "helm_release" "fsx_lustre_csi_driver" {
+  depends_on = [aws_iam_role.fsx_csi_driver_role]
+  name       = "fsx-csi-driver-role"
+  repository = "https://kubernetes-sigs.github.io/aws-fsx-csi-driver"
+  chart      = "aws-fsx-csi-driver"
+  namespace = "kube-system"     
+      
+  set {
+    name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = "${aws_iam_role.fsx_csi_driver_role.arn}"
+  }
+    
+}
